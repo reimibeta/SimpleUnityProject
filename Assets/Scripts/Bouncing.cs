3c467ff1eb474feb103958c5c96154f3bc3ad90f
reimibeta <65539218+reimibeta@users.noreply.github.com>
2020-08-14 21:06:51 +0700

    nothing change

---
+++ Assets/Scripts/Bouncing.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bouncing : MonoBehaviour
+{
+    public static Bouncing instance;
+
+    [SerializeField]
+    private float speed = 20;
+    private Rigidbody rigidBody;
+    void Start()
+    {
+        if (instance == null)
+            instance = this;
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            print("Collision Ground");
+            rigidBody.AddForce(0, speed, 0,ForceMode.Impulse);
+        }
+    }
+}
