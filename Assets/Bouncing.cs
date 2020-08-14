9297bef27a7dc53982cd1784114ae57ff2d72f4f
reimibeta <pinsoneatra.soneatra846@gmail.com>
2020-08-14 17:43:02 +0700

    first commit

---
+++ Assets/Bouncing.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bouncing : MonoBehaviour
+{
+    private Rigidbody rigidbody;
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            print("Ground");
+        }
+    }
+}
